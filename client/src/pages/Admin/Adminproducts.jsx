68051a0c9497503ed237eb58bfd0b52f8d5c2289
Dinesh sharma <dineshsharmadev93@gmail.com>
2025-02-13 10:11:54 +0530

    new changes

---
+++ client/src/pages/Admin/Adminproducts.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import Card from "react-bootstrap/Card";
+// import "../Homeproduct/homeproduct.scss";
+import Select from 'react-select'
+export default function Adminproducts() {
+    const data = [1, 2, 3, 4, 5, 6, 7, 8];
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+  return (
+   <>
+    <div className="home-product">
+      <Container className="mt-4">
+        <Row>
+           <Col md={8}>
+              <h2>All Products</h2>
+           </Col>
+           <Col md={4}>
+               <Select options={options} placeholder="Filter by Category"/>
+           </Col>
+        </Row>
+        <Row>
+          {data.map((element, index) => {
+            return (
+              <>
+                <Col md={4} className="mb-3">
+                  <Card style={{ width: "18rem", height: "" }}>
+                    <Card.Img variant="top" src="/shoes.png" />
+                    <Card.Body>
+                      <Card.Title>Nike Shoes</Card.Title>
+                      <button className=""><i className='fa-solid fa-trash' style={{color:"red"}}></i></button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </>
+            );
+          })}
+        </Row>
+      </Container>
+    </div>
+   </>
+  )
+}
